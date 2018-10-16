246e13b9c845ed5030f250136eb3715fae8544ed
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-16 15:28:00 -0400

    Created basic functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React, { Component } from 'react';
+const App = () => <Amount />;
 
-const App = () => (
-    <div>
-        <h1>Currency Converter</h1>
-    </div>
-);
+class Amount extends Component {
+    constructor(props) {
+        super(props);
 
-export default App;
+        this.state = {
+            amount: 0,
+        };
+    }
+
+    onIncrement = () => {
+        this.setState(state => ({ amount: state.amount + 1 }));
+    };
+
+    onDecrement = () => {
+        this.setState(state => ({ amount: state.amount - 1 }));
+    };
+
+    render() {
+        return (
+            <div>
+                <span>US Dollar: {this.state.amount} </span>
+
+                <button type="button" onClick={this.onIncrement}>
+                    +
+                </button>
+                <button type="button" onClick={this.onDecrement}>
+                    -
+                </button>
+            </div>
+        );
+    }
+}
