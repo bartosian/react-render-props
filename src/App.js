d74498cb37a7278db14a931ad1f3ef17fbaa08bd
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-16 16:29:11 -0400

    Added header to app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const App = () => (
     <Amount>
         {(amount) => (
             <div>
+                <h1>My Currency Converter</h1>
                 <Pound amount={amount} />
                 <Euro amount={amount} />
             </div>
